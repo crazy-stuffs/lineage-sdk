bfd8f82178a6b3485684c81652d6aa8c45605c92
Pranav Vashi <neobuddy89@gmail.com>
2019-11-02 02:06:11 +0530

    sdk: Replace los versioning
    Change-Id: Ie10fea12fed88235c6df901596bd99e9a77c7ad4
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sdk/src/java/lineageos/os/Build.java
+++ sdk/src/java/lineageos/os/Build.java
@@ -28,10 +28,10 @@ public class Build {
     public static final String UNKNOWN = "unknown";
 
     /** A build ID utilized to distinguish lineageos versions */
-    public static final String LINEAGEOS_VERSION = getString("ro.lineage.version");
+    public static final String EVEREST_VERSION = getString("ro.everest.version");
 
     /** A build ID string meant for displaying to the user */
-    public static final String LINEAGEOS_DISPLAY_VERSION = getString("ro.lineage.display.version");
+    public static final String EVEREST_DISPLAY_VERSION = getString("ro.everest.display.version");
 
     private static final SparseArray<String> sdkMap;
     static
